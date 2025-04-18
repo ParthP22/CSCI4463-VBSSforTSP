6edec5ef10f5b79a6f93bf9d54e72812116c28b0
ParthP22 <parthpatel0422@gmail.com>
2025-04-18 12:32:10 -0400

    Successfully reads a .tsp file from the tsp_instances directory. Still needs to be tested on command line

--- src/TSPFileParser.java
+++ src/TSPFileParser.java
@@ -1,3 +1,37 @@
+import java.io.*;
+import java.util.HashMap;
+
 public class TSPFileParser {
 
+    public static HashMap<Integer,double[]> parseTSPFile(String filename) throws IOException {
+        HashMap<Integer,double[]> cities = new HashMap<Integer,double[]>();
+
+        File tspFile = new File(filename);
+
+        FileReader fileReader = new FileReader("./tsp_instances/" + tspFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String line = bufferedReader.readLine();
+
+        while (line != null && !line.equals("NODE_COORD_SECTION")) {
+            line = bufferedReader.readLine();
+        }
+
+        if(line.equals("NODE_COORD_SECTION")){
+            line = bufferedReader.readLine();
+        }
+
+
+
+        while(line != null && !line.equals("EOF")){
+            String[] tokens = line.split(" ");
+            cities.put(Integer.parseInt(tokens[0]),new double[]{Double.parseDouble(tokens[1]),Double.parseDouble(tokens[2])});
+            line = bufferedReader.readLine();
+        }
+
+        bufferedReader.close();
+        fileReader.close();
+
+        return cities;
+    }
 }
