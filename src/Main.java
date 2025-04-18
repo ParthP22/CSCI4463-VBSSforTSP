ed3c95affcb7dd87b9a37f92afb5ebf3712b7f6a
ParthP22 <parthpatel0422@gmail.com>
2025-04-18 13:40:57 -0400

    readjusted main method to use command line args

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
-public class Main {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        VBSS.doVBSS("dj38.tsp","1");
+
+    }
 }
