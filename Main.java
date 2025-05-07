2372902ed5aedc933e3a5ee35579765ae453a499
ParthP22 <parthpatel0422@gmail.com>
2025-05-07 01:57:41 -0400

    The comments in Main.java are now complete

--- Main.java
+++ Main.java
@@ -2,8 +2,18 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The main method of the program in which VBSS is ran,
+ * and it is expected to be run through the command line.
+ * 
+ * @author Parth Patel. Ahmed Malik, James Calabrese
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
+        //args[0] = TSP filename
+        //args[1] = b-parameter value for VBSS
+        //args[2] = number of total iterations of VBSS
+
         VBSS.doVBSS(args[0],args[1],args[2]);
     }
 }
